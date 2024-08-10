29bdfd40d77897f97dd2545bc6620879312bb976
whiteboy13 <masjedi.1386@gmail.com>
2024-08-10 18:43:57 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("btn");
+const result = document.getElementById("pr");
+
+btn.addEventListener("click", () => {
+    console.log("ssssss");
+    result.innerText++
+})
